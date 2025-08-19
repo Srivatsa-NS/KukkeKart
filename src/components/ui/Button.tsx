e7065e81408d8b19ec371f0b64c9f1285fa9e952
Srivatsa NS <srivatsa.ns@snyce.in>
2025-08-19 18:24:53 +0530

    Changed button shadow color to golden

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -5,7 +5,7 @@ function Button({ name, isHovered }: { name: string; isHovered?: boolean }) {
     <div
       className={`w-48 h-16 text-3xl common-font rounded-4xl flex items-center border-3 justify-center hover:cursor-pointer duration-500 shadow-xl ${
         isHovered
-          ? "text-amber-50 bg-amber-800 border-amber-50 shadow-amber-50"
+          ? "text-amber-50 bg-amber-800 border-amber-50 shadow-amber-500"
           : "text-amber-800 bg-amber-50 border-amber-800 shadow-amber-950"
       }`}
     >
