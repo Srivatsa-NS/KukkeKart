f5fc9fdc9fbdf19441c0ae37f45e18bab3c9262b
Srivatsa NS <srivatsa.ns@snyce.in>
2025-08-19 14:39:32 +0530

    Font size and padding changed for menu items

--- src/components/ui/MenuItem.tsx
+++ src/components/ui/MenuItem.tsx
@@ -28,7 +28,7 @@ const MenuItem = ({ label, route }: { label: string; route: string }) => {
         onClick={() => {
           setCurrentPage(route);
         }}
-        className="mr-5 p-3 text-2xl rounded-2xl menu-item common-font hover:cursor-pointer"
+        className="mx-4 px-7 rounded-2xl menu-item common-font hover:cursor-pointer"
       >
         {label}
       </motion.div>
