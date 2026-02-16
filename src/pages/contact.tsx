65954c4003c8999425865318d63d1a4b836e7504
Srivatsa NS <srivatsa.ns@snyce.in>
2026-02-17 00:52:54 +0530

    Map embedding done in contact us page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -246,10 +246,17 @@ function Contact() {
           >
             Find Us
           </h2>
-          <div className="bg-amber-800 rounded-lg h-64 flex items-center justify-center">
-            <p className="body-font text-amber-50 text-lg">
-              Interactive map will be integrated here
-            </p>
+          <div className="bg-amber-800 rounded-lg h-64 overflow-hidden shadow-inner">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6471.245929189673!2d75.61460699665166!3d12.663098838433362!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba4e40b51fd410b%3A0x7a93f9435ccffec2!2sNoochila%2C%20Karnataka%20574238!5e0!3m2!1sen!2sin!4v1771269369362!5m2!1sen!2sin"
+              width="100%"
+              height="100%"
+              style={{ border: 0 }}
+              allowFullScreen={true}
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+              title="KukkeKart Location"
+            ></iframe>
           </div>
         </div>
       </div>
